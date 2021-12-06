c9fe0e8cf2b4f5829b611f6cb050a1c7fd777b52
yvesloicmartin <yvesloicmartin@aaf.lu>
2021-12-06 21:31:18 +0000

    Stripping Hello World

---
+++ create_gitscrap_db.sql
@@ -0,0 +1,18 @@
+-- Database: augur
+
+-- DROP DATABASE IF EXISTS augur;
+
+CREATE DATABASE gitscrap
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF8'
+    LC_CTYPE = 'en_US.UTF8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+GRANT ALL ON DATABASE gitscrap TO scraping;
+
+GRANT ALL ON DATABASE gitscrap TO augur;
+
+GRANT TEMPORARY, CONNECT ON DATABASE gitscrap TO PUBLIC;
