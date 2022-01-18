a4936f10382c57b6987afff6b7a11cd8cddd2426
yvesloicmartin <yvesloicmartin@aaf.lu>
2022-01-18 21:31:18 +0000

    Package project

---
+++ main.py
@@ -0,0 +1,6 @@
+from tests.basic_test import *
+
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [b"GitScrap Welcome"]
+    #test_case_2()
