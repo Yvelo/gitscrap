8451004db5d4359654fb851bf9b89293be1a693f
yvesloicmartin <yvesloicmartin@aaf.lu>
2022-01-18 21:03:52 +0000

    Package project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='GitScrap',
+    version='0.0.1',
+    packages=['tests', 'scraper'],
+    url='https://github.com/Yvelo/gitscrap',
+    license='MIT License',
+    author='Yves-Loic Martin',
+    author_email='yvesloicmartin@aaf.lu',
+    description='Run GitScrap'
+)
