26fec427c78cda50ff14d8037fe7458d11977412
yvesloicmartin <yvesloicmartin@aaf.lu>
2022-01-19 11:06:08 +0000

    Test deployment error in package dependencies

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import requests
+import numpy as np
+import time
+import math
+import psycopg2
+import requests
+import json
+from dotenv import dotenv_values
+from requests.auth import HTTPBasicAuth
+from datetime import datetime
+from time import sleep
+from pprint import pprint
+
 #from tests.basic_test import *
 
 def application(env, start_response):
